9664415fe7aa39476c7dcd4ac425b58f2e1ac563
baschermann <durrean@anti-cheat.com>
2019-01-30 18:09:59 +0100

    Added indication that a team has not yet researched a recipe

---
+++ data.lua
@@ -0,0 +1,25 @@
+data.raw["gui-style"].default["red-background-image-style"] = {
+    type = "button_style",
+    parent = "icon_button",
+    default_graphical_set =
+    {
+        type = "composition",
+        filename = "__split_with_the_team__/graphics/custom_buttons.png",
+        priority = "extra-high-no-scale",
+        corner_size = {3, 3},
+        position = {0, 0}
+    }
+}
+
+data.raw["gui-style"].default["transparent-background-image-style"] = {
+    type = "button_style",
+    parent = "icon_button",
+    default_graphical_set =
+    {
+        type = "composition",
+        filename = "__split_with_the_team__/graphics/custom_buttons.png",
+        priority = "extra-high-no-scale",
+        corner_size = {3, 3},
+        position = {8, 0}
+    }
+}
